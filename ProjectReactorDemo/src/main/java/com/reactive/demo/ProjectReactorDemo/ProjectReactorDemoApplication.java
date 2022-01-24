53774b3fdba261b38e3612a103e4f67acaea2a8b
Amar Singh <amarsingh@outlook.in>
2022-01-24 20:58:22 +0800

    Amar | Initial Commit

---
+++ ProjectReactorDemo/src/main/java/com/reactive/demo/ProjectReactorDemo/ProjectReactorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.demo.ProjectReactorDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectReactorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectReactorDemoApplication.class, args);
+	}
+
+}
---
+++ ProjectReactorDemo/src/test/java/com/reactive/demo/ProjectReactorDemo/ProjectReactorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.demo.ProjectReactorDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectReactorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
